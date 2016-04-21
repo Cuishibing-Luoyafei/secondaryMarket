d2475d0e77d7e3ebe6acae81d7fa10a24eb2c554
luoyafei <635142812@qq.com>
2016-04-21 12:21:38 +0800

    add reply.class and commdity.class

---
+++ src/com/secondaryMarket/dao/impl/CommodityMysqlDao.java
@@ -0,0 +1,32 @@
+package com.secondaryMarket.dao.impl;
+
+import com.secondaryMarket.bean.Commodity;
+import com.secondaryMarket.dao.CommodityDao;
+
+public class CommodityMysqlDao implements CommodityDao {
+
+	@Override
+	public Commodity getCommodityInId(Integer commodityId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean insertCommodity(Commodity commodity) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean updateCommodity(Commodity commodity) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteCommodity(Commodity commodity) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ src/com/secondaryMarket/dao/impl/ReplyMysqlDao.java
@@ -0,0 +1,34 @@
+package com.secondaryMarket.dao.impl;
+
+import java.util.List;
+
+import com.secondaryMarket.bean.Reply;
+import com.secondaryMarket.dao.ReplyDao;
+
+public class ReplyMysqlDao implements ReplyDao {
+
+	@Override
+	public Reply getReplyInId(Integer replyId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Reply> getReplys(Integer start, Integer size) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean insertReply(Reply reply) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteReply(Reply reply) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
