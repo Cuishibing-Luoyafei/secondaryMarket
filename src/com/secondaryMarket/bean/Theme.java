c8a369cb408ef5ddec93ccf86df68a72b8f870b5
Cuishibing <643237029@qq.com>
2016-04-21 21:50:20 +0800

    完成ThemeMysqlDao

--- src/com/secondaryMarket/bean/Theme.java
+++ src/com/secondaryMarket/bean/Theme.java
@@ -1,5 +1,61 @@
 package com.secondaryMarket.bean;
 
-public class Theme {
+import java.sql.Timestamp;
 
+public class Theme {
+	private Integer themeId = -1;
+	private String themeTitle = "";
+	private String themeContent = "";
+	private Timestamp themeTime = null;
+	private Integer themeUserId = -1;
+	private Integer themeCommodityId = -1;
+	private Integer themeIsRead = -1;
+	public Integer getThemeId() {
+		return themeId;
+	}
+	public void setThemeId(Integer themeId) {
+		this.themeId = themeId;
+	}
+	public String getThemeTitle() {
+		return themeTitle;
+	}
+	public void setThemeTitle(String themeTitle) {
+		this.themeTitle = themeTitle;
+	}
+	public String getThemeContent() {
+		return themeContent;
+	}
+	public void setThemeContent(String themeContent) {
+		this.themeContent = themeContent;
+	}
+	public Timestamp getThemeTime() {
+		if(themeTime!=null)
+			return themeTime;
+		else{
+			themeTime = new Timestamp(System.currentTimeMillis());
+			return themeTime;
+		}
+		
+	}
+	public void setThemeTime(Timestamp themeTime) {
+		this.themeTime = themeTime;
+	}
+	public Integer getThemeUserId() {
+		return themeUserId;
+	}
+	public void setThemeUserId(Integer themeUserId) {
+		this.themeUserId = themeUserId;
+	}
+	public Integer getThemeCommodityId() {
+		return themeCommodityId;
+	}
+	public void setThemeCommodityId(Integer themeCommodityId) {
+		this.themeCommodityId = themeCommodityId;
+	}
+	public Integer getThemeIsRead() {
+		return themeIsRead;
+	}
+	public void setThemeIsRead(Integer themeIsRead) {
+		this.themeIsRead = themeIsRead;
+	}
 }
