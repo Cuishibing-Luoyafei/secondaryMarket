d14c71650528aa6beef3fcf097b16d7ba6444bbf
Cuishibing <643237029@qq.com>
2016-05-01 10:53:42 +0800

    写了一部分UploadReply sevlet

--- src/com/secondaryMarket/servlet/UploadReply.java
+++ src/com/secondaryMarket/servlet/UploadReply.java
@@ -1,5 +1,35 @@
 package com.secondaryMarket.servlet;
 
-public class UploadReply {
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.secondaryMarket.factory.ServiceFactory;
+@WebServlet(name="UploadReply",urlPatterns="/UploadReply")
+public class UploadReply extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doPost(req,resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		Boolean isSuccess = false;
+		Boolean isRegister = false;
+		Integer userId = -1;
+		if(req.getSession().getAttribute("userName")==null){
+			isSuccess = false;
+			isRegister = false;
+		}else{
+			isRegister = true;
+			userId = ServiceFactory.createUserService().getUserInName((String)req.getSession().getAttribute("userName")).getUserId();
+			
+		}
+	}
+	
 }
