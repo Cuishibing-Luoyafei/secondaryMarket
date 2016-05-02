ddd31aa05fb11787b229bc21e09ac485c6afb4ea
Cuishibing <643237029@qq.com>
2016-05-02 10:17:32 +0800

    完成UploadReply sevlet

--- src/com/secondaryMarket/servlet/UploadReply.java
+++ src/com/secondaryMarket/servlet/UploadReply.java
@@ -8,7 +8,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.secondaryMarket.bean.Reply;
 import com.secondaryMarket.factory.ServiceFactory;
+import com.secondaryMarket.service.ReplayService;
+
+import net.sf.json.JSONObject;
 @WebServlet(name="UploadReply",urlPatterns="/UploadReply")
 public class UploadReply extends HttpServlet{
 
@@ -22,14 +26,25 @@ public class UploadReply extends HttpServlet{
 		Boolean isSuccess = false;
 		Boolean isRegister = false;
 		Integer userId = -1;
+		JSONObject result = new JSONObject();
 		if(req.getSession().getAttribute("userName")==null){
 			isSuccess = false;
 			isRegister = false;
 		}else{
 			isRegister = true;
 			userId = ServiceFactory.createUserService().getUserInName((String)req.getSession().getAttribute("userName")).getUserId();
-			
+			Integer replyThemeId = Integer.parseInt(req.getParameter("replyThemeId"));
+			String replyContent = req.getParameter("replyContent");
+			ReplayService rs = ServiceFactory.createReplayService();
+			Reply reply = new Reply();
+			reply.setReplyUserId(userId);
+			reply.setReplyContent(replyContent);
+			reply.setReplyThemeId(replyThemeId);
+			isSuccess = rs.insertReply(reply);
 		}
+		result.accumulate("isSuccess", isSuccess.toString());
+		result.accumulate("isRegister", isRegister.toString());
+		resp.getWriter().write(result.toString());
 	}
 	
 }
